f160f431377cc58cabe3460798db171bb2829856
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-02-14 12:57:14 +0100

    Run tests on CI

--- cico_run_tests.sh
+++ cico_run_tests.sh
@@ -4,6 +4,13 @@ set -ex
 
 . cico_setup.sh
 
-build_image
+prep() {
+    yum -y update
+    yum -y install epel-release
+    yum -y install python34 python34-virtualenv which
+}
 
+prep
+build_image
+./runtests.sh
 push_image
---
+++ runtests.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+
+threshold=0.6
+
+export MAJORITY_THRESHOLD=$threshold
+export DATA_DIR=.
+
+function prepare_venv() {
+    VIRTUALENV=`which virtualenv`
+    if [ $? -eq 1 ]; then
+        # python34 which is in CentOS does not have virtualenv binary
+        VIRTUALENV=`which virtualenv-3`
+    fi
+
+    ${VIRTUALENV} -p python3 venv && source venv/bin/activate && python3 `which pip3` install pytest pytest-cov
+}
+
+[ "$NOVENV" == "1" ] || prepare_venv || exit 1
+
+# the module src/config.py must exists because it is included from stack_license and license_analysis.py as well.
+cp src/config.py.template src/config.py
+cd tests
+PYTHONDONTWRITEBYTECODE=1 python3 `which pytest` -vv .
+PYTHONDONTWRITEBYTECODE=1 python3 `which pytest` --cov=../src/ --cov-report term-missing -vv .
