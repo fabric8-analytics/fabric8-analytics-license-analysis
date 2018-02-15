09744b9c03f32da234e2786e14c0da39476cf0ca
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-02-15 10:24:45 +0100

    Move the package installation into cico_setup.sh script

--- cico_run_tests.sh
+++ cico_run_tests.sh
@@ -4,13 +4,6 @@ set -ex
 
 . cico_setup.sh
 
-prep() {
-    yum -y update
-    yum -y install epel-release
-    yum -y install python34 python34-virtualenv which
-}
-
-prep
 build_image
 ./runtests.sh
 push_image
