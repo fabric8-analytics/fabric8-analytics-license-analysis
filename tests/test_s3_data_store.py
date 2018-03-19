5b60aa0f42ac418e6ed0b20cfb8cee1cc43c4043
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-03-19 11:05:48 +0100

    Mocking S3 objects

--- tests/test_s3_data_store.py
+++ tests/test_s3_data_store.py
@@ -1,5 +1,6 @@
 """Tests for the class S3DataStore."""
 
+from unittest.mock import *
 import pytest
 import os
 
@@ -17,7 +18,39 @@ def test_get_name():
     assert s3DataStore.get_name() == "S3:bucket"
 
 
-def test_read_json_file():
+class _MockedS3Data:
+    def __init__(self):
+        pass
+
+    def read(self):
+        return b'{"json":"data"}'
+
+
+class _MockedS3Object:
+
+    def __init__(self):
+        pass
+
+    def get(self):
+        return {"Body": _MockedS3Data()}
+
+
+class _MockedS3Resource:
+    def __init__(self):
+        pass
+
+    def Object(self, bucket_name, filename):
+        return _MockedS3Object()
+
+
+def test_read_json_file_positive():
+    """Check the method read_json_file()."""
+    s3DataStore = S3DataStore("bucket", "access_key", "sercet_key")
+    s3DataStore.s3_resource = _MockedS3Resource()
+    s3DataStore.read_json_file("file")
+
+
+def test_read_json_file_negative():
     """Check the method read_json_file()."""
     # try for improper access_key and secret_key
     s3DataStore = S3DataStore("bucket", "access_key", "sercet_key")
@@ -41,7 +74,15 @@ def test_list_files():
         s3DataStore.list_files()
 
 
-def test_read_all_json_files():
+@patch('src.util.data_store.s3_data_store.S3DataStore.list_files', return_value=['file1', 'file2'])
+def test_read_all_json_files_positive(mocked_object):
+    """Check the method read_all_json_files()."""
+    s3DataStore = S3DataStore("bucket", "access_key", "sercet_key")
+    s3DataStore.s3_resource = _MockedS3Resource()
+    s3DataStore.read_all_json_files()
+
+
+def test_read_all_json_files_negative():
     """Check the method read_all_json_files()."""
     # try for improper access_key and secret_key
     s3DataStore = S3DataStore("bucket", "access_key", "sercet_key")
