b2a4d08b78a29b82d784f88573b7616ded7d90c7
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-05-31 12:37:41 +0200

    New unit tests for stack license: convert_version_to_proper_semantic, select_latest_version, extract_license_outliers etc.

--- tests/test_stack_license.py
+++ tests/test_stack_license.py
@@ -1,7 +1,7 @@
 """Unit tests for the StackLicenseAnalyzer module."""
 
 from unittest.mock import *
-from src.stack_license import StackLicenseAnalyzer
+from src.stack_license import *
 
 
 # single instance of stack license analyzer
@@ -275,7 +275,6 @@ def test_check_compatibility_conflicts():
                 'licenses': ['GPL V3+']
                 }]
     output = stack_license_analyzer._check_compatibility('GPL V3+', packages)
-    print(output)
     assert 'p2' in output['compatible_packages']
     assert 'p1' in output['conflict_packages']
     assert not output['unknown_license_packages']
@@ -304,8 +303,6 @@ def test_stack_licenses_computation():
     }
     output = stack_license_analyzer.compute_stack_license(payload=payload)
     assert output is not None
-    print(output)
-    print(output["outlier_packages"])
     assert output['status'] == 'Successful'
     assert output['stack_license'] == 'cddlv1.1+'
     assert output["outlier_packages"] == {'p1': 'cddlv1.1+'}
@@ -319,7 +316,101 @@ def test_unexpected_exception_handling():
     }
     output = stack_license_analyzer.compute_stack_license(payload=payload)
     assert output is not None
-    print(output)
     assert output['status'] == 'Failure'
     assert output['stack_license'] is None
     assert output['message'] == "Some unexpected exception happened!"
+
+
+def _check_package(package):
+    assert "package" in package
+    assert package["package"] in set(["package1", "package2"])
+
+
+def _check_license(package):
+    assert "license" in package
+    assert package["license"] in set(["license1", "license2"])
+
+
+def test_extract_component_details():
+    """Test the extract_component_details method."""
+    component_details = stack_license_analyzer.extract_component_details({})
+    assert "ecosystem" in component_details
+    assert "name" in component_details
+    assert "version" in component_details
+    assert "licenses" in component_details
+    assert component_details["ecosystem"] == ""
+    assert component_details["name"] == ""
+    assert component_details["version"] == ""
+    assert component_details["licenses"] == []
+
+    component = {
+        "version": {
+            "declared_licenses": ["license1"],
+            "pname": ["The package"],
+            "version": ["1.0.0"],
+            "pecosystem": ["Maven"]
+        }
+    }
+    component_details = stack_license_analyzer.extract_component_details(component)
+    assert "ecosystem" in component_details
+    assert "name" in component_details
+    assert "version" in component_details
+    assert "licenses" in component_details
+    assert component_details["ecosystem"] == "Maven"
+    assert component_details["name"] == "The package"
+    assert component_details["version"] == "1.0.0"
+    assert component_details["licenses"] == ["license1"]
+
+
+def test_extract_license_outliers():
+    """Test the _extract_license_outliers method."""
+    license_outliers = stack_license_analyzer._extract_license_outliers(None)
+    assert license_outliers == []
+
+    data = {
+        "outlier_packages": {
+            "package1": "license1",
+            "package2": "license2"
+        }
+    }
+    license_outliers = stack_license_analyzer._extract_license_outliers(data)
+    assert license_outliers is not None
+    assert len(license_outliers) == 2
+    p1 = license_outliers[0]
+    p2 = license_outliers[1]
+    _check_license(p1)
+    _check_license(p2)
+    _check_package(p1)
+    _check_package(p2)
+
+
+def test_convert_version_to_proper_semantic():
+    """Test the convert_version_to_proper_semantic function."""
+    versions = [
+        ["1", "1"],
+        ["1.2", "1.2"],
+        ["1.2.3", "1.2.3"],
+        ["1.2.3.4", "1.2.3-4"],
+        ["1-2", "1.2"],
+        ["1-2-3", "1.2.3"],
+        ["1-2-3-4", "1.2.3-4"],
+        ["1-2-3-beta", "1.2.3-beta"],
+    ]
+
+    for version in versions:
+        input = version[0]
+        expected = version[1]
+        assert convert_version_to_proper_semantic(input) == expected
+
+
+def test_select_latest_version():
+    """Test the select_latest_version function."""
+    assert select_latest_version() == "0.0.0"
+    assert select_latest_version(input_version="1.0.0") == ""
+    assert select_latest_version(input_version="1.0.0", anitya="1.0.0") == "1.0.0"
+    assert select_latest_version(input_version="1.0.0", libio="1.0.0") == "1.0.0"
+
+    # error handling
+    assert select_latest_version(input_version="") == ""
+    assert select_latest_version(anitya="") == ""
+    assert select_latest_version(libio="") == ""
