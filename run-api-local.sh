1a7f132980dd4d0e56fec0a735d4c4bdda79383c
Tomáš Hrčka <thrcka@redhat.com>
2018-09-11 08:41:28 +0200

    Use venv to run the API locally

--- run-api-local.sh
+++ run-api-local.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+PYTHONPATH=$(pwd)/src/
+export PYTHONPATH
+
+function prepare_venv() {
+    virtualenv -p python3 venv && source venv/bin/activate && python3 "$(which pip3)" install -r requirements.txt && python3 "$(which pip3)" install git+https://github.com/fabric8-analytics/fabric8-analytics-auth.git
+}
+
+[ "$NOVENV" == "1" ] || prepare_venv || exit 1
+
 # Usage info
 # Ref. Link http://mywiki.wooledge.org/BashFAQ/035
 show_help() {
