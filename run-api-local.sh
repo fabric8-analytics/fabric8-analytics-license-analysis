9826d92b72ef0b23fb37ba77233dc59a4013ab26
Tomáš Hrčka <thrcka@redhat.com>
2018-09-11 09:31:42 +0200

    Configure env variables for local run Remove fetch_public_key method

--- run-api-local.sh
+++ run-api-local.sh
@@ -4,7 +4,8 @@ PYTHONPATH=$(pwd)/src/
 export PYTHONPATH
 
 function prepare_venv() {
-    virtualenv -p python3 venv && source venv/bin/activate && python3 "$(which pip3)" install -r requirements.txt && python3 "$(which pip3)" install git+https://github.com/fabric8-analytics/fabric8-analytics-auth.git
+    virtualenv -p python3 venv && source venv/bin/activate && python3 "$(which pip3)" install -r requirements.txt &&
+     python3 "$(which pip3)" install git+https://github.com/fabric8-analytics/fabric8-analytics-auth.git
 }
 
 [ "$NOVENV" == "1" ] || prepare_venv || exit 1
@@ -43,6 +44,9 @@ while getopts ht:p: opt; do
       ;;
   esac
 done
+#setup auth library variables
+export OSIO_AUTH_URL='http://auth.openshift.io'
+export FABRIC8_ANALYTICS_JWT_AUDIENCE='fabric8-online-platform'
 
 export MAJORITY_THRESHOLD=$threshold
 export SERVICE_PORT=$port
