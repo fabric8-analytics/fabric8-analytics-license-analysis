0e12985c4cdbe110a3fe032716141b8d48a3aec8
Deepak Sharma <deepshar@redhat.com>
2021-03-26 14:45:39 +0530

    quay creds in rhel

--- build_deploy.rhel.sh
+++ build_deploy.rhel.sh
@@ -7,6 +7,11 @@ IMG="${BASE_IMG}:latest"
 
 GIT_HASH=`git rev-parse --short=7 HEAD`
 
+# login to quay to pull base image
+DOCKER_CONF="$PWD/.docker"
+mkdir -p "$DOCKER_CONF"
+docker --config="$DOCKER_CONF" login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
+
 # build the image
 docker build  --no-cache \
               --force-rm \
